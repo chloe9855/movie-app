f3e6434bc7e3105c244074b6c266da6a9c8e5598
chloe9855 <liu870707@gmail.com>
2022-04-20 10:14:54 +0800

    alter deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -25,6 +25,6 @@ git commit -m 'deploy'
 # 以這個專案來講就要改成這樣以下這樣，下面是走 ssh 模式
 # git push -f git@github.com/yushinhe/movie.git master:gh-pages
 # 除此之外，也可以改走 HTTPS 模式
-git push -f https://github.com/chloe9855/movie-app.git master:gh-pages
+git push -f https://github.com/chloe9855/movie-app.git main:gh-pages
 
 cd -
